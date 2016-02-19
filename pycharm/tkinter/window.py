752f43aae890597518dab4c2192d5168654a29e7
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-02-19 20:48:59 +0000

    buttons , input

--- pycharm/tkinter/window.py
+++ pycharm/tkinter/window.py
@@ -1,5 +1,23 @@
 from tkinter import *
-myGui = Tk()
-myGui.title('Hello')
-myGui.geometry("500x500+500+300")
-myGui.mainloop()
+mygui = Tk()
+mysecondgui = Tk()
+
+def hello():
+    lastuserinput = userinput.get()
+    mylabel3 = Label(mysecondgui, text=lastuserinput).pack()
+
+
+def dele():
+    mylabel4 = Label(text='deleted').pack()
+
+userinput = StringVar()
+mygui.title('Hello')
+mygui.geometry("450x400+1450+100")
+mysecondgui.title('Second Window')
+mysecondgui.geometry("450x500+1450+600")
+mylabel1 = Label(mygui, text='RESULTS', fg='red', bg='white', font=30).pack()
+mybutton1 = Button(mygui, text='Enter', bg='blue', fg='white',font=20, command = hello ).place(x=200, y=200)
+mybutton2 = Button(mysecondgui, text='Delete', bg='red', fg='white',font=20, command = dele).place(x=300, y=400)
+text = Entry(textvariable=userinput).pack()
+
+mygui.mainloop()
