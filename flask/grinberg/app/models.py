73e607e3c6563c6e18cb56249b63899d1195e78f
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-05-13 20:40:01 +0100

    13 May 2016 Added avatars Amended db to allow more profile info including path to avatar_large / avatar_small Need to upload to gledhill.xyz so images can be found and continue with modifying profile from:  http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vi-profile-page-and-avatars Db migrated successfully

--- flask/grinberg/app/models.py
+++ flask/grinberg/app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from hashlib import md5
+# from hashlib import md5
 
 
 class User(db.Model):
@@ -7,6 +7,9 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    avatar_large = db.Column(db.String(240))
 
     @property
     def is_authenticated(self):
@@ -30,8 +33,8 @@ class User(db.Model):
         return '<User %r>' % (self.nickname)
 
     def avatar(self, size):
-        return 'http://blog.miguelgrinberg.com/static/images/flask-mega-tutorial-part-vi-1.png'
-
+        # return 'http://blog.miguelgrinberg.com/static/images/flask-mega-tutorial-part-vi-1.png'
+        return "images/avatars/steve-passport.jpg"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
