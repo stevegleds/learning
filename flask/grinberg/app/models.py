2af872da639104bbecafa03619eabe0fe6e12e3f
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-05-20 20:09:48 +0100

    20 May 2016 Created followers / following relationships in db Created tests.py file for unit testing

--- flask/grinberg/app/models.py
+++ flask/grinberg/app/models.py
@@ -2,6 +2,11 @@ from app import db
 # from hashlib import md5
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +15,12 @@ class User(db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
     avatar_large = db.Column(db.String(240))
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     @property
     def is_authenticated(self):
@@ -35,6 +46,20 @@ class User(db.Model):
     def avatar(self, size):
         # return 'http://blog.miguelgrinberg.com/static/images/flask-mega-tutorial-part-vi-1.png'
         return "images/avatars/steve-passport.jpg"
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
---
+++ flask/grinberg/tests.py
@@ -0,0 +1,48 @@
+#!flask/bin/python
+import os
+import unittest
+
+from config import basedir
+from app import app, db
+from app.models import User
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert not u1.is_following(u2)
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
